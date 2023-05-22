789f1dbe4b823d028416f2b0d671362116ab2143
EliasSih <eliazsih@gmail.com>
2023-05-22 19:23:27 +0200

    Working bench automation script.

---
+++ psort.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# define the input sizes
+input_sizes=(10 100 1000)
+
+# define the number of processes
+num_processes=4
+
+# programs to run
+programs=(mpiVersion hybridVT sortingHoare sortingLomuto)
+
+for program in "${programs[@]}"
+do
+  # create a new csv file and write the headers
+  echo "size,iteration,result" > "${program}_results.csv"
+  
+  for size in "${input_sizes[@]}"
+  do
+    for iteration in {1..10}
+    do
+      # check if the program is mpiVersion or hybridVT
+      if [ "$program" == "mpiVersion" ] || [ "$program" == "hybridVT" ]
+      then
+        # run the mpi program and get the result
+        result=$(mpirun -np "$num_processes" "./$program" "$size")
+      else
+        # run the sorting program and get the result
+        result=$("./$program" "$size")
+      fi
+
+      # append the result to the csv file
+      echo "$size,$iteration,$result" >> "${program}_results.csv"
+    done
+  done
+done
